d2974f04304bde8e988da4e9b025b1c5f75bd6d4
Erskari <eerosavukoski30@hotmail.com>
2023-10-31 16:33:30 +0200

    Added weatherApp

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,7 +2,21 @@ package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +24,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getWeatherData(View view) {
+        String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=tampere&appid=b7c7111821aa011de5c0a8397c5beaea&units=metric";
+        StringRequest request = new StringRequest(Request.Method.GET, WEATHER_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                parseWeatherJsonAndUpdateUi(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "VERKKOVIRHE", Toast.LENGTH_LONG).show();
+            }
+        });
+        Volley.newRequestQueue(this).add(request);
+    }
+
+    private void parseWeatherJsonAndUpdateUi(String response) {
+        try {
+            JSONObject weatherJSON = new JSONObject(response);
+            String weather = weatherJSON.getJSONArray("weather").getJSONObject(0).getString("main");
+            double temperature = weatherJSON.getJSONObject("main").getDouble("temp");
+            double wind = weatherJSON.getJSONObject("wind").getDouble("speed");
+
+            TextView temperatureTextView = findViewById(R.id.temperatureTextView);
+            temperatureTextView.setText("" + temperature + "C");
+
+            TextView windTextView = findViewById(R.id.windTextView);
+            windTextView.setText("" + wind + "m/s");
+
+            TextView weatherTextView = findViewById(R.id.weatherTextView);
+            weatherTextView.setText(weather);
+        } catch(JSONException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void openForeca(View view) {
+        String foreca = "https://www.foreca.fi";
+        Uri forecaUri = Uri.parse(foreca);
+        Intent intent = new Intent(Intent.ACTION_VIEW, forecaUri);
+        try{
+            startActivity(intent);
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
     }
 }
